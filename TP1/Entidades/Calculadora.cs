162b4c791f6104bfeccfe109e2bb933afb1b9408
matiasnarrua <47338063+matiasnarrua@users.noreply.github.com>
2022-04-17 21:29:58 -0300

    Add files via upload

---
+++ TP1/Entidades/Calculadora.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Entidades
+{
+    public  static class Calculadora
+    {
+
+        private static String ValidarOperador(String operador)
+        {
+            if (operador != "-" && operador != "+" && operador != "/" && operador != "*")
+            {
+                return "+";
+            }
+            else
+            {
+                return operador;
+            }
+        }
+
+        public static double Operar(Operando num1, Operando num2, String operador)
+        {
+            operador = ValidarOperador(operador);
+            double calculo = 0;
+            switch (operador)
+            {
+                case "-":
+                    calculo = num1 - num2;
+                    break;
+                case "+":
+                    calculo = num1 + num2;
+                    break;
+                case "/":
+                    calculo = num1 / num2;
+                    break;
+                case "*":
+                    calculo = num1 * num2;
+                    break;
+            }
+            return calculo;
+        }
+
+
+
+
+    }
+}
